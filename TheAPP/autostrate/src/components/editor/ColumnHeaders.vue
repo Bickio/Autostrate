f9c7af37698bf0912194c6af4433c4fb9e8f62dc
Unknown <bickioocallahan@gmail.com>
2017-05-19 11:26:37 +1200

    Right align text in column headers

--- TheAPP/autostrate/src/components/editor/ColumnHeaders.vue
+++ TheAPP/autostrate/src/components/editor/ColumnHeaders.vue
@@ -1,10 +1,13 @@
 <template lang="html">
 <div class="column is-2">
-  <div class="field">
+  <div class="field has-text-right">
     <h1 class="title">Chords</h1>
   </div>
-  <div class="">
+  <div class="field has-text-right">
     <h1 class="title">Melody</h1>
+  </div>
+  <div class="field has-text-right">
+    <h1 class="title">Rule</h1>
   </div>
 </div>
 </template>
@@ -15,4 +18,5 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
 </style>
