ffdf93b6061bb18c0fb33e91c4275cb128a603bf
Bickio <bickioocallahan@gmail.com>
2017-05-20 13:33:37 +1200

    Fix size of AddBar button

--- TheAPP/autostrate/src/components/editor/AddBar.vue
+++ TheAPP/autostrate/src/components/editor/AddBar.vue
@@ -1,10 +1,13 @@
 <template lang="html">
   <div class="column is-1">
+    <div class="field">
+      <br class="title">
+    </div>
     <button type="button"
             name="button"
             class="button"
             @click="addColumn"
-            style="width: 100%; height: calc(100% - 49px);">Add</button>
+            style="width: 100%; height: 132px">Add</button>
     <hr>
   </div>
 </template>
