785518bdb84de14bee368d57476d92ddb8695ac6
Bickio <bickioocallahan@gmail.com>
2017-05-20 13:55:33 +1200

    Reformat getters and add instruments getter

--- TheAPP/autostrate/src/store/getters.js
+++ TheAPP/autostrate/src/store/getters.js
@@ -1,7 +1,18 @@
-export const currentProjectId = state => state.current_project_id
-export const columns = (state, getters) => state.projects[getters.currentProjectId].columns
-export const columnOrder = (state, getters) => state.projects[getters.currentProjectId].column_order
-export const projectName = (state, getters) => state.projects[getters.currentProjectId].name
-export const projectKey = (state, getters) => state.projects[getters.currentProjectId].key
 export const rules = state => state.rules
-export const projectDefaultRule = (state, getters) => state.projects[getters.currentProjectId].default_rule
+
+export const currentProjectId = state => state.current_project_id
+
+export const projectDefaultRule = (state, getters) =>
+                                  state.projects[getters.currentProjectId].default_rule
+export const projectName = (state, getters) =>
+                           state.projects[getters.currentProjectId].name
+export const projectKey = (state, getters) =>
+                          state.projects[getters.currentProjectId].key
+
+export const columns = (state, getters) =>
+                       state.projects[getters.currentProjectId].columns
+export const columnOrder = (state, getters) =>
+                           state.projects[getters.currentProjectId].column_order
+
+export const instruments = (state, getters) =>
+                           state.projects[getters.currentProjectId].instruments
