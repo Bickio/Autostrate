53beec302f6eeca7cc4035f6953f24624e1f2e75
Unknown <bickioocallahan@gmail.com>
2017-05-22 20:59:29 +1200

    Make drop 2 actually drop the 2...

--- TheAPP/autostrate/src/api/rules/drop_2.js
+++ TheAPP/autostrate/src/api/rules/drop_2.js
@@ -8,11 +8,15 @@ export default {
     let notes = t.chord.notes(chord)
     let i = notes.indexOf(melody) + 1
 
+    // Cheap hack to make sure there are enough notes
+    notes = notes.concat(notes).concat(notes)
+
     notes = notes.slice(i, notes.length).concat(notes.slice(0, i))
-    console.log(notes)
-    if (notes) {
-      return notes
-    }
-    return ['', '', '', '']
+    notes = notes.reverse()
+    notes = notes.slice(0, 4)
+    let droppedNote = notes.splice(1, 1)[0]
+    notes.push(droppedNote)
+
+    return notes
   }
 }
