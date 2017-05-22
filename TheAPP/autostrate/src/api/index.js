703ab141dc1cdb4cdaf3f9549d326813f1680b20
Unknown <bickioocallahan@gmail.com>
2017-05-22 20:58:53 +1200

    Handle emtpy input fields consistently

--- TheAPP/autostrate/src/api/index.js
+++ TheAPP/autostrate/src/api/index.js
@@ -12,10 +12,17 @@ api.isValidChord = function (chord) {
 }
 
 api.isValidNote = function (note) {
+  if (!note) {
+    return true
+  }
   return !!t.note.name(note)
 }
 
 api.voicing = function (chord, melody, rule) {
+  // Check for an empty field
+  if (!chord || !melody) {
+    return Array(10).fill('')
+  }
   return api.rules[rule].makeVoicing(chord, melody)
 }
 export default api
