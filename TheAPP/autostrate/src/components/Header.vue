2e27b37784f9687545bf3fe52ebc6f4cff8c0659
Unknown <bickioocallahan@gmail.com>
2017-06-03 11:00:18 +1200

    Make header responsive and add github link and logo placeholder

--- TheAPP/autostrate/src/components/Header.vue
+++ TheAPP/autostrate/src/components/Header.vue
@@ -1,21 +1,56 @@
 <template lang="html">
   <nav class="nav has-shadow">
-    <div class="container">
-      <div class="nav-left">
-        <a class="nav-item title is-3">
-          Autostrate
-        </a>
-      </div>
-      <div class="nav-right nav-menu">
-        <router-link class="nav-item is-tab" to="/" exact>Home</router-link>
-        <router-link class="nav-item is-tab" to="/editor">Editor</router-link>
-      </div>
+    <div class="nav-left">
+      <a class="nav-item">
+        <img src="http://via.placeholder.com/640x160?text=Logo" alt="Bulma logo">
+      </a>
+    </div>
+
+    <div class="nav-center">
+      <a class="nav-item"
+         href="https://github.com/Bickio/Autostrate"
+         target="_blank">
+        <span class="icon">
+          <i class="fa fa-github"></i>
+        </span>
+      </a>
+    </div>
+
+    <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
+    <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
+    <span class="nav-toggle" @click="navMenuOn = !navMenuOn">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+
+    <!-- This "nav-menu" is hidden on mobile -->
+    <!-- Add the modifier "is-active" to display it on mobile -->
+    <div class="nav-right nav-menu"
+         :class="{'is-active': navMenuOn}"
+         @click="navMenuOn = !navMenuOn">
+      <router-link :to="'/'"
+                   exact
+                   class="nav-item is-tab"
+                   tag='a'>
+        Home
+      </router-link>
+      <router-link :to="'/editor'"
+                   class="nav-item is-tab"
+                   tag='a'>
+        Editor
+      </router-link>
     </div>
   </nav>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      navMenuOn: false
+    }
+  }
 }
 </script>
 
