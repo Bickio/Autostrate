ac9f2121f6c5513ccac6bbc1a07c09f183692998
Unknown <bickioocallahan@gmail.com>
2017-07-11 14:52:08 +1200

    Add a mockup of Projects and ProjectCard

---
+++ TheAPP/autostrate/src/components/projects/ProjectCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        Name
+      </p>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        <div class="columns">
+          <div class="column is-6">
+            <h5>Key: Am</h5>
+            <h5>Length: 5</h5>
+          </div>
+          <div class="column is-6">
+            <h5>Instruments:</h5>
+            <ul>
+              <li>Trumpet</li>
+              <li>Alto Sax</li>
+              <li>Tenor Sax</li>
+              <li>Trombone</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a class="card-footer-item">Open</a>
+      <a class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProjectCard'
+}
+</script>
+<style scoped>
+</style>
--- TheAPP/autostrate/src/components/projects/Projects.vue
+++ TheAPP/autostrate/src/components/projects/Projects.vue
@@ -1,13 +1,34 @@
 <template>
-  
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <app-project-card></app-project-card>
+        </div>
+        <div class="column">
+          <app-project-card></app-project-card>
+        </div>
+        <div class="column">
+          <app-project-card></app-project-card>
+        </div>
+        <div class="column">
+          <app-project-card></app-project-card>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import ProjectCard from './ProjectCard'
 export default {
   name: 'Projects',
   data: () => ({
 
-  })
+  }),
+  components: {
+    'app-project-card': ProjectCard
+  }
 }
 </script>
 
