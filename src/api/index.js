f5a2f7a409f825173d3930021aef7b38f08d6331
Unknown <bickioocallahan@gmail.com>
2017-09-06 13:29:35 +1200

    Fix up api/index with comments

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,12 @@ import t from 'tonal'
 import drop2 from './rules/drop_2'
 import close from './rules/close'
 
+const IMPORTED_RULES = [drop2, close]
+
+/**
+ * Creates a rules object with unique ids from a list of rules.
+ * @param {array} ruleList - The list of rules.
+ */
 function makeRules (ruleList) {
   let rules = {}
   for (let i = 0; i < ruleList.length; i++) {
@@ -13,35 +19,63 @@ function makeRules (ruleList) {
 }
 
 export default class Api {
+  /**
+   * Initialise the class with a rules object
+   * @constructor
+  */
   constructor () {
-    this._rules = makeRules([drop2, close])
+    this._rules = makeRules(IMPORTED_RULES)
   }
 
-  static isValidChord (chord) {
-    return t.chord.isKnownChord(chord)
+  /**
+   * Checks if a string is a valid chord
+   * @param {string} text - The string to be checked
+  */
+  static isValidChord (text) {
+    return t.chord.isKnownChord(text)
   }
 
-  static isValidNote (note) {
-    if (!note) {
+  /**
+   * Checks if a string is a valid note
+   * Beware that a blank string IS valid so as not to overwhelm the user with
+   red before they even start
+   * @param {string} text - The string to be checked
+  */
+  static isValidNote (text) {
+    if (!text) { // If text is blank
       return true
     }
-    return !!t.note.name(note)
+    return !!t.note.name(text) // Return whether t.note.name(text) is truthy
   }
 
-  static isValidKey (key) {
-    return this.isValidNote(key) && key
+  /**
+   * Checks if a string is a valid key (transposition)
+   * @param {string} text - The string to be checked
+  */
+  static isValidKey (text) {
+    return this.isValidNote(text) && text
   }
 
+  /**
+   * Transposes a note from middle c to a given key
+   * @param {string} text - The string to be checked
+  */
   transposeNote (note, key) {
-    let refNote = teoria.note('C4')
-    if (teoria.note(key).octave() === 2) {
-      refNote.transpose(teoria.interval('P8').direction('down'))
+    // Sets a reference note
+    var refNote = teoria.note('C4')
+    // If the key is in the second octave, put it down two octaves
+    if ((teoria.note(key).octave() === 2) && (key[-1] !== '2')) {
       refNote.transpose(teoria.interval('P8').direction('down'))
     }
     let interval = teoria.interval.between(teoria.note(key), refNote)
     return note.transpose(interval)
   }
 
+  /**
+   * Returns a new voicing where the notes are transposed according to the .key property of the intruments.
+   * @param {array} voicing - the list of notes.
+   * @param {object} instruments - An object containing instruments.
+   */
   transposeVoicing (voicing, instruments) {
     let transposed = {}
     for (let id in instruments) {
@@ -50,31 +84,45 @@ export default class Api {
     return transposed
   }
 
+  /**
+   * Returns true if all input is valid or empty otherwise returns false
+   * @param {string} chord - name of chord
+   * @param {string} melody - melody note
+   * @param {string} rule - the rule id
+   * @param {object} instruments - An object containing instruments.
+   */
   inputIsInvalid (chord, melody, rule, instruments) {
     let numberOfInstruments = Object.keys(instruments).length
     // Check for an empty field or unselected rule
     if (!chord || !melody || !rule) {
-      // Output a blank output for each instrument
       return true
     }
-    // If any of the keys are blank output a blank voicing
+    // If any of the keys are blank
     for (let i of Object.keys(instruments)) {
       if (!instruments[i].key) {
         return true
       }
     }
+    // If both the chord and note are valid
     if (!t.chord.isKnownChord(chord) || !t.note.name(melody)) {
       return true
     }
     // If the number of instruments is not supported by the rule output blank
     let validNumbersOfInstruments = this._rules[rule].number_of_instruments
-    // indexOf returns 0 (false) if not found
+    // Note: Array.indexOf(item) returns 0 (false) if not found
     if (validNumbersOfInstruments.indexOf(numberOfInstruments)) {
       throw new Error('This rule needs ' + validNumbersOfInstruments + ' instruments')
     }
     return false
   }
 
+  /**
+   * Returns a finished voicing created from the inputs:
+   * @param {string} chord - name of chord
+   * @param {string} melody - melody note
+   * @param {string} rule - the rule id
+   * @param {object} instruments - An object containing instruments.
+   */
   voicing (chord, melody, rule, instruments) {
     if (this.inputIsInvalid(chord, melody, rule, instruments)) {
       return Array(numberOfInstruments).fill('')
@@ -101,7 +149,9 @@ export default class Api {
 
     return voicing
   }
-
+  /**
+   * Gets the rules (object)
+   */
   get rules () {
     return this._rules
   }
