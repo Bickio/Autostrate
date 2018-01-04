4a1f3facfe115044fa85e98fe45b4fff86277a8f
Unknown <bickioocallahan@gmail.com>
2018-01-04 15:42:30 +1300

    AlignHeaders

--- src/components/editor/MobileHeaders.vue
+++ src/components/editor/MobileHeaders.vue
@@ -15,11 +15,11 @@
       <h1 class="title has-text-right"
           style="margin-right: 24px;">Rule</h1>
     </div>
-    <div class="field" style="height: 36px; padding-top: 12px;">
+    <div class="field" style="height: 21px; margin-top: 12px; margin-bottom: 20px;">
       <h1 class="title is-4 has-text-right"
           style="margin-right: 24px;">Instruments</h1>
     </div>
-    <div style="margin-bottom: 24px; height: 45px;"
+    <div style="margin-bottom: 21px; height: 45px; padding-top: 7px;"
          v-for="id in instrumentOrder">
       <h1 class="title has-text-right"
           style="margin-right: 24px;">{{instruments[id].name}}</h1>
@@ -41,4 +41,7 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.field {
+  height: 36px;
+}
 </style>
